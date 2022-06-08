6f222831c2710283bc0fde63ba66f8394cc82299
Raúl González Alvarado <gonzalv@ciencias.unam.mx>
2022-06-08 15:48:50 -0500

    Agrega esqueleto para cli

---
+++ src/pkg/cli/cli.py
@@ -0,0 +1,9 @@
+from ..logic import encriptar, desencriptar
+
+def run():
+    """Ejecuta la interfaz de línea de comandos (CLI).
+
+    Valida los parámetros y manda llamar las funcionalidades base
+    de la aplicación.
+    """
+    pass

73fcdbfe7164b1616b54f6f7d6c7400f6555f011
Raúl González Alvarado <gonzalv@ciencias.unam.mx>
2022-06-08 15:42:02 -0500

    Agrega esqueleto para input output

---
+++ src/pkg/io/__init__.py
@@ -0,0 +1,3 @@
+from .io_keys import save_keys
+from .io_keys import read_keys
+from .get_password import get_password
---
+++ src/pkg/io/io_keys.py
@@ -0,0 +1,33 @@
+def save_keys(file_path, keys):
+    """Guarda las llaves en el archivo ubicado en file_path
+
+    Las llaves deben ser una lista de tuplas. Cada tupla
+    representa un punto. Ej: [(1,3), (8,9), (14, 123), (28, 2)]
+
+    Parameters:
+    -----------
+    file_path: str
+        El path del archivo donde se van a guardar los puntos
+    keys: list of (tuples of int)
+        Los puntos a guardar
+    """
+    pass
+
+def read_keys(file_path):
+    """Obtiene los puntos almacenados en el archivo ubicado
+    en file_path.
+
+    Las llaves obtenidas deben ser una lista de tuplas. Cada tupla
+    representa un punto. Ej: [(1,3), (8,9), (14, 123), (28, 2)]
+
+    Parameters:
+    -----------
+    file_path: str
+        El path del archivo donde se encuentras los puntos
+
+    Returns:
+    --------
+    list of (tuples of int)
+        Las llaves leidas
+    """
+    pass

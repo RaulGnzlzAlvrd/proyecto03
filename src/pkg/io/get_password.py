b835ccd5ecbc5ff0196370cf0c898725a402815b
miriaamcamargo <miriamcamargo@ciencias.unam.mx>
2022-06-14 15:22:37 -0500

    Se agrega funcion para obtener contraseña

--- src/pkg/io/get_password.py
+++ src/pkg/io/get_password.py
@@ -1,3 +1,5 @@
+import getpass
+
 def get_password():
     """Obtiene una contraseña desde la línea de comandos
     ingresada por el usuario. La contraseña no se 
@@ -8,4 +10,12 @@ def get_password():
     str:
         La contraseña ingresada por el usuario
     """
-    return "password"
+    try:
+        p = getpass.getpass(prompt= 'Ingresa tu contraseña', stream=None)
+
+    except Exception as err:
+        print('ERROR:', err)
+    else:
+        return p
+
+get_password()
